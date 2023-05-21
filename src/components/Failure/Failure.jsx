f21e6629dabe1a0a49652e8cf71c62c79b531581
Samarth Rao <raosamarth1234@gmail.com>
2023-05-21 18:55:40 +0530

    Fix error message

--- src/components/Failure/Failure.jsx
+++ src/components/Failure/Failure.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Failure() {
   return (
-    <div><h2>Failed to get weather details</h2></div>
+    <div><h2>Enter correct City</h2></div>
   )
 }
